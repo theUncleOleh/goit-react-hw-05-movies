3c4ea1e726d6ec4444c1e578e4598000ecddf9e0
theUncleOleh <77876967+theUncleOleh@users.noreply.github.com>
2022-05-20 17:34:31 +0000

    Deploying to gh-pages from @ theUncleOleh/goit-react-hw-05-movies@53b1b0c25b5d9d569210c8152e53a447af590c59 🚀

---
+++ static/js/not-found.b03460b7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[942],{4520:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(3504),o=n(184);function a(){return(0,o.jsxs)("h1",{children:["404 not found page :( transfer on ",(0,o.jsx)(r.rU,{to:"/",children:"Home page"})]})}}}]);
+//# sourceMappingURL=not-found.b03460b7.chunk.js.map
